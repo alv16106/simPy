bdf10bce1fcf02c9e76f8e4646ac8b6f5b6968ea
Rodrigo Alvarado <alv16106@uvg.edu.gt>
2017-03-02 22:39:18 -0600

    Debbuging
    Añadido el seed al random y el contador de tiempo

--- xasdfa.py
+++ xasdfa.py
@@ -5,14 +5,13 @@
 
 import simpy
 import random
-from time import time
 
-class globales:
+class G:
     rtot=0
-tiempo_inicial = time() 
+    tot=0
+
 def x(nombre,env, ram, cpu):
-   
-   
+    inicio=env.now
     inst = random.randint(1,10)
     
     llegada = random.expovariate(1.0/10)
@@ -22,7 +21,7 @@ def x(nombre,env, ram, cpu):
     memoriaPedida = random.randint(1,10)
     print('%s pidio %d de RAM'% (nombre, memoriaPedida))
     yield env.timeout(memoriaPedida)
-    globales.rtot=globales.rtot+memoriaPedida
+    G.rtot=G.rtot+memoriaPedida
     flag=True
     while flag:
         if (100-ram.level)>=memoriaPedida:
@@ -45,7 +44,7 @@ def x(nombre,env, ram, cpu):
             if (inst<=0):
                 break
             else:
-                inst=inst-10
+                inst=inst-1
                 destino=random.randint(1,2)
                 if (destino==1):
                     io=random.randint(1,10)
@@ -57,25 +56,21 @@ def x(nombre,env, ram, cpu):
             
     print('%s libero %d de RAM'% (nombre, memoriaPedida))
     yield ram.put(memoriaPedida)
-        
+    final = env.now
+    tiempo = final-inicio
+    G.tot=G.tot + tiempo
 
+
+random.seed(10)
 env = simpy.Environment() #ambiente de simulación
 ram = simpy.Container(env, init=10, capacity = 100)
 cpu = simpy.Resource(env, capacity=1)
 
-
-for i in range(50):
+for i in range(10):
     env.process(x(' %d'%i,env, ram, cpu))
-
    
-env.run(until = 200)
-print(globales.rtot)
-
-tiempo_final = time()
-
-tiempo_ejecucion = tiempo_final - tiempo_inicial
-
-print 'El tiempo de ejecucion fue:',tiempo_ejecucion #En segundos
+env.run(until = 1000)
+print(G.tot)
 
 
 
